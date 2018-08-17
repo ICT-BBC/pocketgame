a296913bcb53530ec2957cd33e0e8b10b158fb69
Roland Rytz <roland.rytz@gmail.com>
2018-08-17 16:10:06 +0200

    add indication of direction to player figures

--- js/game.js
+++ js/game.js
@@ -107,13 +107,21 @@ function Graphics(game, canvas, canvasContainer){
 		context.clearRect(0, 0, canvas.width, canvas.height);
 		for(var player of game.players){
 			context.beginPath();
-			//context.moveTo(player.pos.x, player.pos.y);
 			context.arc(
 				player.pos.x,
 				player.pos.y,
 				20,
 				0,
 				2 * Math.PI
+			);
+			context.moveTo(
+				 player.pos.x + Math.sin(player.angle)*7
+				,player.pos.y - Math.cos(player.angle)*7
+			);
+			
+			context.lineTo(
+				 player.pos.x + Math.sin(player.angle)*27
+				,player.pos.y - Math.cos(player.angle)*27
 			);
 			context.lineWidth = 3;
 			context.fillStyle = player.color;
